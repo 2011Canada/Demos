d96258fb175993e6f93c65c5e4d426b48fce36d8
Abatson <abatson94@gmail.com>
2021-01-15 12:17:17 -0500

    fixed address

--- 5Week/entertainment720/src/remote/DM-paper/index.ts
+++ 5Week/entertainment720/src/remote/DM-paper/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 //this is where we config every single request for a certain api
 export const dmPaperBaseClient = axios.create({
-    baseURL:"http://localhost:10000",
+    baseURL:"http://ec2-3-239-17-176.compute-1.amazonaws.com:10000",
     headers:{
         'Content-Type': "application/json"
     }

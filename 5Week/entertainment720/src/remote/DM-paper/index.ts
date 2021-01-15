74d367135dbf87b1e934dbff231b3af083ff6cc2
Abatson <abatson94@gmail.com>
2021-01-15 12:02:27 -0500

    front end dockerfile

--- 5Week/entertainment720/src/remote/DM-paper/index.ts
+++ 5Week/entertainment720/src/remote/DM-paper/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 //this is where we config every single request for a certain api
 export const dmPaperBaseClient = axios.create({
-    baseURL:"http://localhost:8080",
+    baseURL:"http://localhost:10000",
     headers:{
         'Content-Type': "application/json"
     }
